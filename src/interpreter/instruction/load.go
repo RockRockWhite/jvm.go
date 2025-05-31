d31b78b42ffc7f174c2a3a3f454ada615890b9ca
RockRockWhite <1834309151@qq.com>
2025-05-31 15:30:56 +0000

    [interpreter] iload iload_n inst

--- src/interpreter/instruction/load.go
+++ src/interpreter/instruction/load.go
@@ -3,11 +3,57 @@ package instruction
 import "github.com/RockRockWhite/jvm.go/src/runtime_data"
 
 type ILoadInstBase struct {
-	ByteOprandInstruction
 }
 
-func (inst *ILoadInstBase) Execute(frame *runtime_data.Frame) error {
-	idx := int(inst.Operand)
-	
+func (inst *ILoadInstBase) execute(frame *runtime_data.Frame, idx uint16) error {
+	local_variable := frame.LocalVariables.GetInt(idx)
 
+	frame.OperandStack.Push(local_variable)
+	return nil
+}
+
+type ILoadInst struct {
+	ByteOprandInstruction
+	ILoadInstBase
+}
+
+func (inst *ILoadInst) Execute(frame *runtime_data.Frame) error {
+	idx := uint16(inst.Operand)
+	return inst.execute(frame, idx)
+}
+
+type ILoad0Inst struct {
+	NoOperandsInstruction
+	ILoadInstBase
+}
+
+func (inst *ILoad0Inst) Execute(frame *runtime_data.Frame) error {
+	return inst.execute(frame, 0)
+}
+
+type ILoad1Inst struct {
+	NoOperandsInstruction
+	ILoadInstBase
+}
+
+func (inst *ILoad1Inst) Execute(frame *runtime_data.Frame) error {
+	return inst.execute(frame, 1)
+}
+
+type ILoad2Inst struct {
+	NoOperandsInstruction
+	ILoadInstBase
+}
+
+func (inst *ILoad2Inst) Execute(frame *runtime_data.Frame) error {
+	return inst.execute(frame, 2)
+}
+
+type ILoad3Inst struct {
+	NoOperandsInstruction
+	ILoadInstBase
+}
+
+func (inst *ILoad3Inst) Execute(frame *runtime_data.Frame) error {
+	return inst.execute(frame, 3)
 }
