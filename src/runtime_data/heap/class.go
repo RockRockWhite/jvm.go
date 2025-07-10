a93296e1de1bc6f17c96d2e227b3a72d627fcb9f
RockRockWhite <1834309151@qq.com>
2025-07-10 15:16:12 +0000

    [class_file] rename AccessFlag to AccessFlags

--- src/runtime_data/heap/class.go
+++ src/runtime_data/heap/class.go
@@ -8,30 +8,30 @@ type Field struct {
 type Method struct {
 }
 
-type AccessFlag uint16
+type AccessFlags uint16
 
 const (
-	AccessFlagPublic       AccessFlag = 1
-	AccessFlagPrivate      AccessFlag = 1 << 1
-	AccessFlagProtected    AccessFlag = 1 << 2
-	AccessFlagStatic       AccessFlag = 1 << 3
-	AccessFlagFinal        AccessFlag = 1 << 4
-	AccessFlagSynchronized AccessFlag = 1 << 5
-	AccessFlagVolatile     AccessFlag = 1 << 6
-	AccessFlagBridge       AccessFlag = 1 << 6
-	AccessFlagTransient    AccessFlag = 1 << 7
-	AccessFlagVarargs      AccessFlag = 1 << 7
-	AccessFlagNative       AccessFlag = 1 << 8
-	AccessFlagInterface    AccessFlag = 1 << 9
-	AccessFlagAbstract     AccessFlag = 1 << 10
-	AccessFlagStrict       AccessFlag = 1 << 11
-	AccessFlagSynthetic    AccessFlag = 1 << 12
-	AccessFlagAnnotation   AccessFlag = 1 << 13
-	AccessFlagEnum         AccessFlag = 1 << 14
+	AccessFlagPublic       AccessFlags = 1
+	AccessFlagPrivate      AccessFlags = 1 << 1
+	AccessFlagProtected    AccessFlags = 1 << 2
+	AccessFlagStatic       AccessFlags = 1 << 3
+	AccessFlagFinal        AccessFlags = 1 << 4
+	AccessFlagSynchronized AccessFlags = 1 << 5
+	AccessFlagVolatile     AccessFlags = 1 << 6
+	AccessFlagBridge       AccessFlags = 1 << 6
+	AccessFlagTransient    AccessFlags = 1 << 7
+	AccessFlagVarargs      AccessFlags = 1 << 7
+	AccessFlagNative       AccessFlags = 1 << 8
+	AccessFlagInterface    AccessFlags = 1 << 9
+	AccessFlagAbstract     AccessFlags = 1 << 10
+	AccessFlagStrict       AccessFlags = 1 << 11
+	AccessFlagSynthetic    AccessFlags = 1 << 12
+	AccessFlagAnnotation   AccessFlags = 1 << 13
+	AccessFlagEnum         AccessFlags = 1 << 14
 )
 
 type Class struct {
-	AccessFlags      AccessFlag
+	AccessFlags      AccessFlags
 	Name             string
 	SuperClassName   string
 	InterfaceNames   []string
