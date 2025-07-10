47920a69ad7e2de44c8dcf168a37f59ef1cdac0b
RockRockWhite <1834309151@qq.com>
2025-07-10 15:06:44 +0000

    [class_loader] AccessFlags

--- src/runtime_data/heap/class.go
+++ src/runtime_data/heap/class.go
@@ -8,8 +8,30 @@ type Field struct {
 type Method struct {
 }
 
+type AccessFlag uint16
+
+const (
+	AccessFlagPublic       AccessFlag = 1
+	AccessFlagPrivate      AccessFlag = 1 << 1
+	AccessFlagProtected    AccessFlag = 1 << 2
+	AccessFlagStatic       AccessFlag = 1 << 3
+	AccessFlagFinal        AccessFlag = 1 << 4
+	AccessFlagSynchronized AccessFlag = 1 << 5
+	AccessFlagVolatile     AccessFlag = 1 << 6
+	AccessFlagBridge       AccessFlag = 1 << 6
+	AccessFlagTransient    AccessFlag = 1 << 7
+	AccessFlagVarargs      AccessFlag = 1 << 7
+	AccessFlagNative       AccessFlag = 1 << 8
+	AccessFlagInterface    AccessFlag = 1 << 9
+	AccessFlagAbstract     AccessFlag = 1 << 10
+	AccessFlagStrict       AccessFlag = 1 << 11
+	AccessFlagSynthetic    AccessFlag = 1 << 12
+	AccessFlagAnnotation   AccessFlag = 1 << 13
+	AccessFlagEnum         AccessFlag = 1 << 14
+)
+
 type Class struct {
-	AccessFlags      uint16
+	AccessFlags      AccessFlag
 	Name             string
 	SuperClassName   string
 	InterfaceNames   []string
